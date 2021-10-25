54bb78f00e5ba97b23c98e2738e41c6b7b85e698
KJSoo95 <wltn7star@naver.com>
2021-10-25 16:57:04 +0900

    -

--- 007.파일럿PJ/002.구현소스/js/main.js
+++ 007.파일럿PJ/002.구현소스/js/main.js
@@ -44,9 +44,24 @@ $(function(){
     // 현재 슬라이드 위치값 구하기
     // 슬라이드 위치값
     let spos;
+    //이징변수
+    let easing = "easeOutQuint";
+    // 화면커버(광드래그막기)
+    let cover = $(".cover");
+    // 슬라이드 순번 변수
+    let sno = 0; // 첫 슬라이드는 0번(블릿li순번도 0번부터!)
+    // 블릿요소
+    let indic = $(".bindic li");
+    // 슬라이드 개스
+    let scnt = slide.find("li").length;
+    console.log(scnt);  
     // 대상 : .slide - > slide변수
     // 이벤트 : dragstop -> 드래그가 끝날때
     slide.on("dragstop", function(){
+
+        // 광드래그 막기 커버보임
+        cover.show();
+
         // 슬라이드 위치값 구하기
         spos = slide.offset().left;
         // offset().left 화면 왼쪽기준선 left위치
@@ -58,9 +73,25 @@ $(function(){
         if(spos < -win*1.1){
 
             /// 슬라이드가  -200% 위치로 이동
-            slide.animate({
+            // stop() 메서드는 animate가 큐에 쌓이는 것을 막는다!
+            slide.stop().animate({
                 left : -win*2 + "px"
-            }, 700); ////////////// animate //////////////
+            }, 700, easing, function(){ // 콜백함수(이동 후)
+                // 첫번째 슬라이드 맨뒤로 보내기
+                slide.append(slide.find("li").first())
+                // 이때 left값을 -100%위치로 고정해야함!
+                .css({left : -win + "px"});
+
+                // 광드래그 커버 지우기
+                cover.hide();
+            }); ////////////// animate //////////////
+
+            // 블릿순번변경하기 : 오른쪽이동은 증가!
+            sno++;
+            // 한계수 : 슬라이드 수와 같아지면 첫번호로!
+            if(sno === 6) sno =0;
+
+            chgIndic();
             
         } ////////////////////////// if문 : -100%보다 작을때 ///////////////////////
         
@@ -69,27 +100,50 @@ $(function(){
         else if(spos >  -win*0.9){
 
             /// 슬라이드가  0 위치로 이동
-            slide.animate({
+            slide.stop().animate({
                 left :  "0px"
-            }, 700); ////////////// animate //////////////
+            }, 700, easing, function(){ // 콜백함수
+                slide.prepend(slide.find("li").last())
+                // left값을 원래 위치인 -100%값으로 바꿔준다
+                .css({left : -win + "px" });
+                cover.hide();
+
+            }); ////////////// animate //////////////
+
+            // 블릿순서변경하기 : 왼쪽이동
+
+            sno--;
+            if(sno ===-1) sno=scnt-1;
             
+            chgIndic();
+
         } ////////////////////////// if문 : -90%보다 클때 ///////////////////////
         
         // 3. 사이범위 일때 제자리로 돌아오기
         // -110% < 범위 < -90%
         else{
-            slide.animate({
+            slide.stop().animate({
                 left : -win + "px"
-            }, 300); ////////////// animate //////////////
+            }, 300, function(){
+                cover.hide();
+
+            }); ////////////// animate //////////////
             
         } /////////////////////////// else 문 : 사이범위
+        
+       
+       
 
 
     });//////////////////////////// drag ///////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////
 
     
-
+ // 블릿변경함수
+        let chgIndic = () => {
+            // 블릿변경하기 : .blindic li -> indic변수
+            indic.eq(sno).addClass("on").siblings().removeClass("on");
+        };
 
 
 
---
+++ 009.Ajax학습/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 첫페이지</title>
+    <!-- php 코딩영역 -->
+    <?php 
+        // 주석
+        /*  주석  */
+        # 주석
+
+        // php 변수는 문자앞에 $를 씀
+        $link = "<a href = 'vogue/'>보그바로가기</a>";
+        $link2 = "<a href = 'PHP_include'>인클루드</a>";
+        // 화면 출력 -> echo 메시지
+        echo "<h1> 나의 첫 PHP페이지!</h1>";
+        echo"<h2>$link</h2>";
+        echo"<h2>$link2</h2>";
+    
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
